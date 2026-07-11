39e361080049def687c764b76eecec6ac0cbf555
Shagun Aggarwal <shagunaggarwal31714@gmail.com>
2026-07-11 16:28:47 +0530

    remove debug test files from build

--- apps/backend/test_actual.ts
+++
@@ -1,3 +0,0 @@
-import { CRMRecordSchema } from "./src/utils/zodSchemas";
-
-console.log("null:", JSON.stringify(CRMRecordSchema.safeParse({ country_code: null }), null, 2));
--- apps/backend/test_shape.ts
+++
@@ -1,5 +0,0 @@
-import { CRMRecordSchema } from "./src/utils/zodSchemas";
-
-for (const [key, schema] of Object.entries(CRMRecordSchema.shape)) {
-  console.log(`${key}:`, schema.constructor.name);
-}
